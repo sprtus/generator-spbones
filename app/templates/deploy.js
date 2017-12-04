7c268b072eb6dd21e26bcb39f89897b39c8b7a9b
david-orc <david@oldrivercreative.com>
2017-12-04 05:53:09 -0800

    Creating a Sharepoint Yeoman project to facilitate the start of SP projects.

---
+++ app/templates/deploy.js
@@ -0,0 +1,33 @@
+// dependencies
+const spsave = require('spsave').spsave
+
+// load env.js configuration file
+const env = require('./env')
+
+// configuration options
+const config = {
+  siteUrl: env.site,
+  checkin: false,
+  checkinType: 2,
+  checkinMessage: 'Automatic deployment via SPBones'
+}
+
+// file options
+const files = {
+  glob: 'dist/_catalogs/**/*.*',
+  base: '_catalogs',
+  folder: ''
+}
+
+// credentials for SP connection
+const credentials = {
+  username:env.username,
+  password:env.password
+}
+
+// deploy
+spsave(config, credentials, files).then(function(){
+  console.log('Deploy to SharePoint complete')
+}).catch(function(err){
+  console.log(`Error while deploying to SharePoint: ${err}`)
+})
